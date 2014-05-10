d292cc599a5460ecebf4a9344815e0ac3f8ab302
sulivanDotEU <benjamin.ellis@sulivan.eu>
2014-05-10 19:33:28 +0200

    J rai reussi a faire un submodule et a l'importer comme il le fallait =D

--- app/autoload.php
+++ app/autoload.php
@@ -4,13 +4,51 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
 
-require_once(__DIR__ . '/../vendor/symfony/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php');
-
+//require_once(__DIR__ . '/../vendor/symfony/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php');
+//
+//use Symfony\Component\ClassLoader\UniversalClassLoader;
+//
+//$loader = new UniversalClassLoader();
+//$loader->registerNamespaces(array(
+//    'Walva\\CrudGeneratorBundle' => __DIR__.'/../src/vendor/walva/CrudGeneratorBundle',
+//));
+//
+//$loader->register();
 /**
  * @var ClassLoader $loader
  */
+
+
+
+require_once __DIR__."/../vendor/symfony/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php";
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+ 
+$loader = new UniversalClassLoader();
+$loader->registerNamespace("Walva", __DIR__.'/../src/vendor/');
+$loader->register();
+
+
+
+
+
+
+
+
+
+
+
+
+
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+
+//$loader->add('Walva\\CrudGeneratorBundle', __DIR__.'/../src/vendor/walva/CrudGeneratorBundle');
+/*
+$loader->registerNamespaces(array(
+    'Walva\\CrudGeneratorBundle' => __DIR__.'/../src/vendor/walva/CrudGeneratorBundle',
+    ));
+*/
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
 
 return $loader;
